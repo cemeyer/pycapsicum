37c861bfc6ad75f368f01b558cbfb3a94fb0ba90
Conrad Meyer <cem@FreeBSD.org>
2020-03-28 14:12:22 -0700

    Implement test case decorator for subprocess sandbox tests
    With the @util._process_isolate decorator, each so-annotated test is run in
    an independent process.  Some care is taken to ensure remote test failure
    tracebacks are accurately transmitted back to the testrunner without extra
    noise from the remote mechanism showing up or impacting test results.
    
    You would think this would be trivial, but Python does not allow pickling
    traceback objects for no particularly good reason.
    
    The decorator is a crude hack and might be broken by future versions of
    Python, but it works in 2.7 and 3.7 in my testing.

---
+++ tests/util.py
@@ -0,0 +1,130 @@
+# Copyright 2020 Conrad Meyer <cem@FreeBSD.org>
+#
+# SPDX-License-Identifier: WTFNMFPL-1.0
+
+import functools
+import linecache
+import multiprocessing as mp
+import re
+import sys
+import traceback
+
+
+def _do_nothing(*args):
+    pass
+
+
+# One-off monkey-patch of format_exception ...
+__format_exception = traceback.format_exception
+def _format_exception(etype, value, tb, limit=None):
+    msg = ['Traceback (most recent call last):\n']
+
+    tbdat = []
+    for i, (fname, lno, methnam, linestr) in enumerate(value.tbdat):
+        # Elide implementation details that don't help debug test failures.
+        if i == 0 and methnam == "_remote_wrapper":
+            continue
+        # Py unittest result _exc_info_to_string attempts to skip the
+        # implementation of assertions.  This method is more crude, but
+        # whatever.
+        elif re.match(r'.*thon\d.\d\/unittest\/', fname):
+            continue
+
+        if linestr is None:
+            linestr = linecache.getline(fname, lno)
+            if linestr:
+                linestr = linestr.strip()
+            else:
+                linestr = None
+
+        tbdat.append((fname, lno, methnam, linestr))
+
+    result = traceback.format_list(tbdat)
+    result += traceback.format_exception_only(etype, value)
+    traceback.format_exception = __format_exception
+    return result
+
+
+# Py3000 rototills traceback and uses something called TracebackException instead.
+if sys.version_info >= (3, 0):
+    _TracebackException = traceback.TracebackException
+    class MonkeyPatchTraceBack(traceback.TracebackException):
+        def __init__(self, etype, value, *args, **kwargs):
+            super().__init__(etype, value, *args, **kwargs)
+            self.value = value
+            traceback.TracebackException = _TracebackException
+
+        def format(self, *args, **kwargs):
+            for x in _format_exception(self.exc_type, self.value, None):
+                yield x
+
+
+# In a remote process, run the test and send pickled test result (None for
+# success, exception for all other conditions) back to the runner.
+def _remote_wrapper(target, args, resultq):
+    try:
+        if sys.version_info >= (3, 2):
+            # Avoid useless noise ResourceWarning on Py3000 for ephemeral test
+            # processes:
+            import warnings
+            warnings.filterwarnings("ignore", "", ResourceWarning, "", 0)
+
+        # Preopen linecache so that exception traces have (some) code contents
+        # from this end of things.  We can't predict in advance what files will
+        # be needed, and can't open files after capsicum entry.
+        if sys.version_info >= (3, 0):
+            # Another gratuitous Python3 API change ...
+            fnam = target.__code__.co_filename
+        else:
+            fnam = target.func_code.co_filename
+
+        # Cache lines from the file of the test case itself.  Any missing lines
+        # we'll be able to fill on the test-runner side afterwards (assuming
+        # files contents don't change racily).
+        linecache.getlines(fnam)
+
+        # Prevent linecache from destroying its cache when it can't stat the
+        # files from the sandbox.
+        linecache.checkcache = _do_nothing
+
+        # Run the dang wrapped test!
+        target(*args)
+    except:
+        # Can't pickle tracebacks, so extract most metadata and pickle that
+        # instead.
+        einfo = sys.exc_info()
+        einfo = einfo[:2] + (traceback.extract_tb(einfo[2]),)
+
+        resultq.put(einfo)
+    else:
+        resultq.put(None)
+
+
+# Decorator: Run a test in a subprocess, to isolate effects (principally,
+# capsicum sandboxing).
+def _process_isolate(f):
+    @functools.wraps(f)
+    def wrapper(test, subprocess=False):
+        q = mp.Queue()
+        p = mp.Process(target=_remote_wrapper, args=(f, (test,), q))
+        p.start()
+        p.join()
+
+        # XXX Notably missing:
+        # 1. Handle of remote process death for any reason that isn't a Python
+        # exception or otherwise doesn't get serialized into anything for this
+        # Queue.  (E.g., fatal signal.)
+        # 2. Any sort of timeout mechanism.  Granted, we don't have that
+        # anyway.
+        res = q.get_nowait()
+        if res is None:
+            return
+
+        # Re-raise exception
+        if sys.version_info >= (3, 0):
+            traceback.TracebackException = MonkeyPatchTraceBack
+        else:
+            traceback.format_exception = _format_exception
+        res[1].tbdat = res[2]
+        raise res[1]
+    return wrapper
